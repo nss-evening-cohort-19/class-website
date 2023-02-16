c60baee357fd5610dfd1d4fce720a3d10ddaa0a5
Rochelle Mikel Rossman <102299152+rochelle-rossman@users.noreply.github.com>
2023-02-16 12:37:43 -0600

    changes to data.photoUrls

--- utils/ViewDirector.js
+++ utils/ViewDirector.js
@@ -1,16 +1,14 @@
 import PropTypes from 'prop-types';
 import NavBar from '../components/NavBar';
 
-const ViewDirectorBasedOnUserAuthStatus = ({ component: Component, pageProps }) => {
-    return (
-      <>
-        <NavBar /> 
-        <div className="container">
-          <Component {...pageProps} />
-        </div>
-      </>
-    );
-};
+const ViewDirectorBasedOnUserAuthStatus = ({ component: Component, pageProps }) => (
+  <>
+    <NavBar />
+    <div className="container">
+      <Component {...pageProps} />
+    </div>
+  </>
+);
 
 export default ViewDirectorBasedOnUserAuthStatus;
 
