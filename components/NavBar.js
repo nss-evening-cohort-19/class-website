a77ca42124cf4727533f3fe9145c10fdfb3c2dfc
Justin Ferwerda <justin.ferwerda@gmail.com>
2023-02-16 10:20:11 -0600

    added logo and navbar links

--- components/NavBar.js
+++ components/NavBar.js
@@ -1,3 +1,4 @@
+/* eslint-disable @next/next/no-img-element */
 /* eslint-disable jsx-a11y/anchor-is-valid */
 import React from 'react';
 import Link from 'next/link';
@@ -5,11 +6,9 @@ import Link from 'next/link';
 export default function NavBar() {
   return (
     <nav className="navbar navbar-expand-md navbar-dark bg-dark">
-      <div className="container-fluid">
-        <Link passHref href="/">
-          <a className="navbar-brand" data-bs-toggle="collapse" data-bs-target="#navbarTogglerDemo01">
-            CHANGE ME
-          </a>
+      <div className="container-fluid nav-buttons">
+        <Link href="/" passHref>
+          <img src="/images/nsslogo.png" className="d-inline-block align-top logo" alt="logoimage" />
         </Link>
         <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarTogglerDemo01" aria-controls="navbarTogglerDemo01" aria-expanded="false" aria-label="Toggle navigation">
           <span className="navbar-toggler-icon" />
@@ -21,6 +20,27 @@ export default function NavBar() {
               <Link passHref href="/">
                 <a className="nav-link">
                   Home
+                </a>
+              </Link>
+            </li>
+            <li className="nav-item">
+              <Link passHref href="#developers">
+                <a href="#developers" className="nav-link">
+                  Developers
+                </a>
+              </Link>
+            </li>
+            <li className="nav-item">
+              <Link passHref href="#Technologies">
+                <a className="nav-link">
+                  Technologies
+                </a>
+              </Link>
+            </li>
+            <li className="nav-item">
+              <Link passHref href="#Thanks">
+                <a className="nav-link">
+                  Thanks
                 </a>
               </Link>
             </li>
